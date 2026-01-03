f61a9954f60f67b4f68cd255e0e14ecd149213f7
Athit Yuthaworawit <48763974+GlarZzzz@users.noreply.github.com>
2026-01-03 17:34:02 +0700

    Update Demo_tb_Adder_Base_Resize.v

--- Demo_tb_Adder_Base_Resize.v
+++ Demo_tb_Adder_Base_Resize.v
@@ -30,7 +30,7 @@ module Demo_tb_Adder_Base_Resize;
     localparam DST_WIDTH  = 640;
     localparam DST_HEIGHT = 480;
     localparam FRAC_BITS  = 16;
-    localparam INPUT_PIXEL_FILE  = "verilog_input_pixels_rgb_interleaved.hex";
+    localparam INPUT_PIXEL_FILE  = "input_pixels_rgb.hex";
     localparam OUTPUT_PIXEL_FILE = "verilog_output_pixels.txt";
     localparam TOTAL_SRC_PIXELS = SRC_WIDTH * SRC_HEIGHT;
     localparam TOTAL_DST_PIXELS = DST_WIDTH * DST_HEIGHT;
